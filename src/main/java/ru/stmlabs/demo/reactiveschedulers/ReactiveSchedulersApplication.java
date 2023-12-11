aca71e58d40b1f547ceb56eb4feb4e043e48c295
Dmitrii Rodionov <sf.zeon@gmail.com>
2023-12-11 15:53:46 +0300

    Add DB service and check threads switching

--- src/main/java/ru/stmlabs/demo/reactiveschedulers/ReactiveSchedulersApplication.java
+++ src/main/java/ru/stmlabs/demo/reactiveschedulers/ReactiveSchedulersApplication.java
@@ -1,8 +1,5 @@
 package ru.stmlabs.demo.reactiveschedulers;
 
-import static ru.stmlabs.demo.reactiveschedulers.utils.ThreadUtils.logCurrentThread;
-import static ru.stmlabs.demo.reactiveschedulers.utils.ThreadUtils.sleepCurrentThreadForOneSecond;
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -13,32 +10,42 @@ import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import ru.stmlabs.demo.reactiveschedulers.service.DataBaseService;
 
+import java.time.Duration;
+
+import static ru.stmlabs.demo.reactiveschedulers.utils.ThreadUtils.logCurrentThread;
+
 @Slf4j
 @AllArgsConstructor
 @SpringBootApplication
 public class ReactiveSchedulersApplication implements CommandLineRunner {
 
-  public final ApplicationContext context;
+    private final ApplicationContext context;
 
-  private static final Scheduler MAIN_SCHEDULER = Schedulers.newSingle("MAIN_THREAD");
+    private static final Scheduler FIRST_MAIN_SCHED = Schedulers.newSingle("FIRST_MAIN_SCHED");
+    private static final Scheduler SECOND_MAIN_SCHED = Schedulers.newSingle("SECOND_MAIN_SCHED");
+    private static final Scheduler THIRD_MAIN_SCHED = Schedulers.newSingle("THIRD_MAIN_SCHED");
 
-  public static void main(String[] args) {
-    SpringApplication.run(ReactiveSchedulersApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSchedulersApplication.class, args);
+    }
 
-  @Override
-  public void run(String... args) {
-    var dbService = context.getBean(DataBaseService.class);
-    logCurrentThread("Before the stream");
-    dbService.fetchTheAnswerOfLifeUniverseAndEverything(MAIN_SCHEDULER)
-//        .publishOn(MAIN_SCHEDULER)
-        .doOnNext(data -> logCurrentThread("Process the answer"))
-        .subscribeOn(MAIN_SCHEDULER)
-        .subscribe();
-    logCurrentThread("After the stream");
+    @Override
+    public void run(String... args) {
+        DataBaseService service = context.getBean(DataBaseService.class);
 
-    sleepCurrentThreadForOneSecond();
-    DataBaseService.DB_SCHEDULER.dispose();
-    MAIN_SCHEDULER.dispose();
-  }
+        service.fetchDataAsync(FIRST_MAIN_SCHED)
+//                .publishOn(FIRST_MAIN_SCHED)
+                .delayElement(Duration.ofMillis(100))
+                .doOnNext(data -> logCurrentThread("Got data!"))
+                .subscribe();
+
+//        Mono.just(1)
+//                .doOnNext(data -> logCurrentThread("doOnNext " + data))
+//                .publishOn(FIRST_MAIN_SCHED)
+//                .doOnNext(data->logCurrentThread("doOnNext2 " + data))
+//                .publishOn(THIRD_MAIN_SCHED)
+//                .doOnNext(data->logCurrentThread("doOnNext3"))
+//                .subscribeOn(SECOND_MAIN_SCHED)
+//                .subscribe();
+    }
 }
